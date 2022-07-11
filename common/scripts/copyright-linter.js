3472c4ab66a306c7775e5f4caa815b7839b70dfa
Mr_GaoYu <Mr_GaoYu@163.com>
2022-07-11 13:27:04 +0800

    rush update

---
+++ common/scripts/copyright-linter.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+const child_process = require("child_process");
+
+console.log(process.argv, 112321)
+
+const filePaths = process.argv.reduce((acc, cur) => {
+
+}, false)
+
+if (filePaths) {
+
+}
